53ab0ae4a62a8f2c49421d3062d23089aa23ef36
Yang Wang <yang@1536-laptop.local>
2018-05-28 04:56:19 -0700

    sync

---
+++ Keras-FCN/run.py
@@ -0,0 +1,103 @@
+import sys, skvideo.io, json, base64
+import numpy as np
+from PIL import Image
+from io import BytesIO, StringIO
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+import os
+import sys
+import pickle
+from keras.optimizers import SGD, Adam, Nadam
+from keras.callbacks import *
+from keras.objectives import *
+from keras.metrics import binary_accuracy
+from keras.models import load_model
+import keras.backend as K
+#import keras.utils.visualize_util as vis_util
+
+from models import *
+from utils.loss_function import *
+from utils.metrics import *
+from utils.SegDataGenerator import *
+import time
+import sklearn
+from sklearn.model_selection import train_test_split
+
+def zerg_model(weight_decay=0., batch_momentum=0.9, batch_shape=[1, 320, 320, 3], classes=2):
+    if batch_shape:
+        img_input = Input(batch_shape=batch_shape)
+        image_size = batch_shape[1:3]
+    else:
+        print ('zerg_model should have batch_shape')
+        exit()
+
+    bn_axis = 3
+
+    x = Conv2D(64, (7, 7), strides=(2, 2), padding='same', name='conv1', kernel_regularizer=l2(weight_decay))(img_input)
+    x = BatchNormalization(axis=bn_axis, name='bn_conv1', momentum=batch_momentum)(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+    x = conv_block(3, [64, 64, 256], stage=2, block='a', weight_decay=weight_decay, strides=(1, 1), batch_momentum=batch_momentum)(x)
+    x = identity_block(3, [64, 64, 256], stage=2, block='b', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
+    x = identity_block(3, [64, 64, 256], stage=2, block='c', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
+
+    x = conv_block(3, [128, 128, 512], stage=3, block='a', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
+    x = identity_block(3, [128, 128, 512], stage=3, block='b', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
+    x = identity_block(3, [128, 128, 512], stage=3, block='c', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
+    x = identity_block(3, [128, 128, 512], stage=3, block='d', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
+
+    x = conv_block(3, [256, 256, 1024], stage=4, block='a', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
+    x = identity_block(3, [256, 256, 1024], stage=4, block='b', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
+    x = identity_block(3, [256, 256, 1024], stage=4, block='c', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
+    x = identity_block(3, [256, 256, 1024], stage=4, block='d', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
+    x = identity_block(3, [256, 256, 1024], stage=4, block='e', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
+    x = identity_block(3, [256, 256, 1024], stage=4, block='f', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
+
+    x = atrous_conv_block(3, [512, 512, 2048], stage=5, block='a', weight_decay=weight_decay, atrous_rate=(2, 2), batch_momentum=batch_momentum)(x)
+    x = atrous_identity_block(3, [512, 512, 2048], stage=5, block='b', weight_decay=weight_decay, atrous_rate=(2, 2), batch_momentum=batch_momentum)(x)
+    x = atrous_identity_block(3, [512, 512, 2048], stage=5, block='c', weight_decay=weight_decay, atrous_rate=(2, 2), batch_momentum=batch_momentum)(x)
+    
+    x = Conv2D(classes, (1, 1), kernel_initializer='he_normal', activation='linear', padding='same', strides=(1, 1), kernel_regularizer=l2(weight_decay))(x)
+    x = BilinearUpSampling2D(target_size=tuple(image_size))(x)
+
+    model = Model(img_input, x)
+    return model
+
+file = sys.argv[-1]
+
+if file == 'demo.py':
+  print ("Error loading video")
+  quit
+
+# Define encoder function
+def encode(array):
+	pil_img = Image.fromarray(array)
+	buff = BytesIO()
+	pil_img.save(buff, format="PNG")
+	return base64.b64encode(buff.getvalue()).decode("utf-8")
+
+video = skvideo.io.vread(file)
+
+answer_key = {}
+
+# Frame numbering starts at 1
+frame = 1
+
+for rgb_frame in video:
+	
+    rgb = cv2.resize(rgb_frame, (320, 320), interpolation = cv2.INTER_CUBIC)
+
+    seg = model.predict(rgb.reshape(1,320,320,3))
+    seg = seg.reshape(320,320,2)
+    seg_road = (seg[:,:,0] > 0.5).astype(np.uint8)
+    seg_vehicle = (seg[:,:,1] > 0.5).astype(np.uint8)
+
+	answer_key[frame] = [encode(seg_vehicle), encode(seg_road)]
+    
+    # Increment frame
+	frame+=1
+
+# Print output in proper json format
+print (json.dumps(answer_key))
