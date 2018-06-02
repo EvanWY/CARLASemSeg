c2b222fb8fe0bd2d8802cf8bd5c8c6d137210a57
Yang Wang <yang@1536-laptop.local>
2018-06-02 01:43:34 -0700

    sync

--- Keras-FCN/inference_client.py
+++ Keras-FCN/inference_client.py
@@ -9,15 +9,13 @@ if file == 'inference_client.py':
 context = zmq.Context()
 
 #  Socket to talk to server
-print("Connecting to hello world server…")
+#print("Connecting to hello world server…")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
-print("connected")
+#print("connected")
 
-print("Sending file name: " + file)
+#print("Sending file name: " + file)
 socket.send_string(file)
-print("File name sent, waiting for respond ... ")
-message = socket.recv_string()
-print("Done")
-
-print (message)
+#print("File name sent, waiting for respond ... ")
+print (socket.recv_string())
+#print("Done")
