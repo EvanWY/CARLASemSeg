49d9e752c1620477167b432695c4d46a71c0bbeb
Yang Wang <yang@1536-laptop.local>
2018-06-02 00:39:06 -0700

    sync

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 git -C /Users/yang/Work/CARLASemSeg add .
 git -C /Users/yang/Work/CARLASemSeg commit -m "sync"
 git -C /Users/yang/Work/CARLASemSeg push
-ssh -i ~/Desktop/lyft_challenge.pem -t ubuntu@$URL "cd /home/workspace/CARLASemSeg; sudo git pull"
+ssh -i ~/Desktop/lyft_challenge.pem -t ubuntu@$EC2 "cd /home/workspace/CARLASemSeg; sudo git pull"
 
 say 'sync done'
