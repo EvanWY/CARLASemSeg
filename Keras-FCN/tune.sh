ac8dcc59808665e6e7a9a9d5b2688d4cc9377bea
Yang Wang <yang@1536-laptop.local>
2018-06-03 01:20:42 -0700

    sync

--- Keras-FCN/tune.sh
+++ Keras-FCN/tune.sh
@@ -4,7 +4,7 @@ do
     ROAD_TH=$i
     VEH_TH=0.5
     echo "testing with ROAD_TH=$ROAD_TH, VEH_TH=$VEH_TH"
-    echo "WY_echo" | grader "python run.py $ROAD_TH $VEH_TH" | grep "Car F score:" | (echo -n "road_th: $ROAD_TH | vehicle_th: $VEH_TH " && cat) >> log_hyper_tune.txt
+    echo "WY_echo" | grader "python run.py $ROAD_TH $VEH_TH" | grep "Car F score:" | (echo -n "road_th: $ROAD_TH | vehicle_th: $VEH_TH | " && cat) >> log_hyper_tune.txt
 done 
 
 for i in `seq 0 0.1 1`;
@@ -12,5 +12,5 @@ do
     ROAD_TH=0.5
     VEH_TH=$i
     echo "testing with ROAD_TH=$ROAD_TH, VEH_TH=$VEH_TH"
-    echo "WY_echo" | grader "python run.py $ROAD_TH $VEH_TH" | grep "Car F score:" | (echo -n "road_th: $ROAD_TH | vehicle_th: $VEH_TH " && cat) >> log_hyper_tune.txt
+    echo "WY_echo" | grader "python run.py $ROAD_TH $VEH_TH" | grep "Car F score:" | (echo -n "road_th: $ROAD_TH | vehicle_th: $VEH_TH | " && cat) >> log_hyper_tune.txt
 done 
