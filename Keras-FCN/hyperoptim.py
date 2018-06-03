4b908cb64b7382701f06eb712676c6119a514093
Yang Wang <yang@1536-laptop.local>
2018-06-03 00:17:43 -0700

    sync

---
+++ Keras-FCN/hyperoptim.py
@@ -0,0 +1,71 @@
+import sys, skvideo.io, json, base64
+import numpy as np
+from PIL import Image
+from io import BytesIO, StringIO
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+import os
+import sys
+import pickle
+from keras.optimizers import SGD, Adam, Nadam
+from keras.callbacks import *
+from keras.objectives import *
+from keras.metrics import binary_accuracy
+from keras.models import load_model
+import keras.backend as K
+#import keras.utils.visualize_util as vis_util
+
+from models import *
+from utils.loss_function import *
+from utils.metrics import *
+from utils.SegDataGenerator import *
+import time
+import sklearn
+from sklearn.model_selection import train_test_split
+
+file = sys.argv[-1]
+
+if file == 'run.py':
+    print ("Error loading video")
+    quit
+
+print sys.argv
+
+# Define encoder function
+def encode(array):
+    pil_img = Image.fromarray(array)
+    buff = BytesIO()
+    pil_img.save(buff, format="PNG")
+    return base64.b64encode(buff.getvalue()).decode("utf-8")
+
+video = skvideo.io.vread(file)
+
+answer_key = {}
+
+# Frame numbering starts at 1
+frame = 1
+
+model = zerg_model(batch_shape=[1, 320, 320, 3])
+model.load_weights('zerg_model.h5')
+
+for rgb_frame in video:
+    
+    img = cv2.resize(rgb_frame, (320, 320), interpolation = cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    seg = model.predict(img.reshape(1,320,320,3))
+    seg = seg.reshape(320,320,2)
+    seg_road = (seg[:,:,0] > 0.6).astype(np.uint8)
+    seg_vehicle = (seg[:,:,1] > 0.3).astype(np.uint8)
+    
+    seg_road_fullsize = cv2.resize(seg_road, (800, 600), interpolation = cv2.INTER_NEAREST)
+    seg_vehicle_fullsize = cv2.resize(seg_vehicle, (800, 600), interpolation = cv2.INTER_NEAREST)
+
+    answer_key[frame] = [encode(seg_vehicle_fullsize), encode(seg_road_fullsize)]
+    
+    # Increment frame
+    frame+=1
+
+# Print output in proper json format
+print (json.dumps(answer_key))
--- hi.py
+++ hi.py
@@ -1,13 +1,15 @@
 import cv2
 import numpy as np
-import sys
+import sys, os
+from subprocess import call
 
-img = cv2.imread('/Users/yang/Desktop/img0001790.png')
-img2 = cv2.imread('/Users/yang/Desktop/seg0001790.png')
+# img = cv2.imread('/Users/yang/Desktop/img0001790.png')
+# img2 = cv2.imread('/Users/yang/Desktop/seg0001790.png')
 
-cv2.imshow('image1', img)
-cv2.imshow('image2', img2 * 20)
+# cv2.imshow('image1', img)
+# cv2.imshow('image2', img2 * 20)
 
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+print os.popen('echo "WY_echo" | grader "python hyperoptim.py {0} {1}"'.format()).read()
