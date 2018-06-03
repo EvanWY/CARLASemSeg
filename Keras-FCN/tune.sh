75b110f364cafc35304fb9ae14d288d48d54811c
Yang Wang <yang@1536-laptop.local>
2018-06-03 13:59:47 -0700

    sync

--- Keras-FCN/tune.sh
+++ Keras-FCN/tune.sh
@@ -1,10 +1,13 @@
 #!/bin/bash 
-for i in `seq 0.5 0.01 0.6`;
+for i in `seq 0 0.1 1`;
 do
     ROAD_TH=0.57
-    VEH_TH=0.20
+    ROAD_FADE=0
+
+    VEH_TH=0.40
+    VEH_FADE=$i
     echo "testing with ROAD_TH=$ROAD_TH, VEH_TH=$VEH_TH"
-    echo "WY_echo" | grader "python run.py $ROAD_TH $VEH_TH" | grep "Car F score:" | (echo -n "road_th: $ROAD_TH | vehicle_th: $VEH_TH | " && cat) >> log_hyper_tune.txt
+    echo "WY_echo" | grader "python inference_client.py $ROAD_TH $VEH_TH $ROAD_FADE $VEH_FADE" | grep "Car F score:" | (echo -n "road_th: $ROAD_TH | road_fade: $ROAD_FADE | veh_th: $VEH_TH | veh_fade: $VEH_FADE | " && cat) >> log_hyper_tune.txt
 done 
 
 #
