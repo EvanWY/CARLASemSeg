624040c7dffa332a3351f34fd6727a7759f4114b
Yang Wang <yang@1536-laptop.local>
2018-06-03 00:47:34 -0700

    sync

--- Keras-FCN/hyperoptim.py
+++ Keras-FCN/hyperoptim.py
@@ -1,23 +1,21 @@
 import os
 import numpy as np
 
-for x in np.arange(0.0, 1.0, 0.1):
-    road_th = x
-    veh_th = 0.5
-    log = os.popen('echo "WY_echo" | grader "python run.py {0} {1}"'.format(road_th, veh_th).read()
+def execute(road_th, veh_th):
+    cmd = 'echo "WY_echo" | grader "python run.py {0} {1}"'.format(road_th, veh_th)
+    print ('[running command]: ' + cmd)
+    log = os.popen(cmd).read()
     for line in log.splitlines():
         if line.startswith("Car F score:"):
             with open("hyperparam_optimization.log", "a") as log_file:
                 text = 'road_th: {0} | veh_th: {1} | '.format(road_th, veh_th) + line
+                print ('[result]: ' + text)
                 log_file.write(text)
 
+
 for x in np.arange(0.0, 1.0, 0.1):
-    road_th = 0.5
-    veh_th = x
-    log = os.popen('echo "WY_echo" | grader "python run.py {0} {1}"'.format(road_th, veh_th).read()
-    for line in log.splitlines():
-        if line.startswith("Car F score:"):
-            with open("hyperparam_optimization.log", "a") as log_file:
-                text = 'road_th: {0} | veh_th: {1} | '.format(road_th, veh_th) + line
-                log_file.write(text)
+    execute(x, 0.5)
+
+for x in np.arange(0.0, 1.0, 0.1):
+    execute(0.5, x)
         
