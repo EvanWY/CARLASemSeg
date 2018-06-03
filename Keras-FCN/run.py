b832b3f2ed97715f7d0bcc1f2d297fec8cf35144
Yang Wang <yang@1536-laptop.local>
2018-06-02 23:55:51 -0700

    sync

--- Keras-FCN/run.py
+++ Keras-FCN/run.py
@@ -54,8 +54,8 @@ for rgb_frame in video:
 
     seg = model.predict(img.reshape(1,320,320,3))
     seg = seg.reshape(320,320,2)
-    seg_road = (seg[:,:,0] > 0.5).astype(np.uint8)
-    seg_vehicle = (seg[:,:,1] > 0.5).astype(np.uint8)
+    seg_road = (seg[:,:,0] > 0.6).astype(np.uint8)
+    seg_vehicle = (seg[:,:,1] > 0.3).astype(np.uint8)
     
     seg_road_fullsize = cv2.resize(seg_road, (800, 600), interpolation = cv2.INTER_NEAREST)
     seg_vehicle_fullsize = cv2.resize(seg_vehicle, (800, 600), interpolation = cv2.INTER_NEAREST)
