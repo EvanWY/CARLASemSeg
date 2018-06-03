3ec2e0f2b7519beedb58013e5af47d8284db1a6c
Yang Wang <yang@1536-laptop.local>
2018-06-03 01:10:54 -0700

    sync

--- Keras-FCN/tune.sh
+++ Keras-FCN/tune.sh
@@ -1,3 +1,3 @@
 ROAD_TH = 0.746
 VEH_TH = 0.344
-echo "WY_echo" | grader "python run.py $ROAD_TH $VEH_TH" | (echo -n "road_th: $ROAD_TH | vehicle_th: $VEH_TH" && cat)
+echo "WY_echo" | grader "python run.py $ROAD_TH $VEH_TH" | (echo -n "road_th: $ROAD_TH | vehicle_th: $VEH_TH" && cat) >> log.txt
