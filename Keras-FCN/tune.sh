57741b8f13e34e93540cfe0257262327daef04fe
Yang Wang <yang@1536-laptop.local>
2018-06-03 01:15:01 -0700

    sync

--- Keras-FCN/tune.sh
+++ Keras-FCN/tune.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
-ROAD_TH = 0.746
-VEH_TH = 0.344
-echo "WY_echo" | grader "python run.py $ROAD_TH $VEH_TH" | grep "Car F score:"" | (echo -n "road_th: $ROAD_TH | vehicle_th: $VEH_TH" && cat) >> log_hyper_tune.txt
+ROAD_TH=0.746
+VEH_TH=0.344
+echo "WY_echo" | grader "python run.py $ROAD_TH $VEH_TH" | grep "Car F score:" | (echo -n "road_th: $ROAD_TH | vehicle_th: $VEH_TH" && cat) >> log_hyper_tune.txt
