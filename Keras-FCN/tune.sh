1bd7a54ea0a68d9ecf4e6e44557adecec47a07c1
Yang Wang <yang@1536-laptop.local>
2018-06-03 02:59:10 -0700

    sync

--- Keras-FCN/tune.sh
+++ Keras-FCN/tune.sh
@@ -1,12 +1,4 @@
 #!/bin/bash 
-for i in `seq 0.1 0.01 0.3`;
-do
-    ROAD_TH=0.55
-    VEH_TH=$i
-    echo "testing with ROAD_TH=$ROAD_TH, VEH_TH=$VEH_TH"
-    echo "WY_echo" | grader "python run.py $ROAD_TH $VEH_TH" | grep "Car F score:" | (echo -n "road_th: $ROAD_TH | vehicle_th: $VEH_TH | " && cat) >> log_hyper_tune.txt
-done 
-
 for i in `seq 0.5 0.01 0.6`;
 do
     ROAD_TH=$i
@@ -14,3 +6,12 @@ do
     echo "testing with ROAD_TH=$ROAD_TH, VEH_TH=$VEH_TH"
     echo "WY_echo" | grader "python run.py $ROAD_TH $VEH_TH" | grep "Car F score:" | (echo -n "road_th: $ROAD_TH | vehicle_th: $VEH_TH | " && cat) >> log_hyper_tune.txt
 done 
+
+# for i in `seq 0.1 0.01 0.3`;
+# do
+#     ROAD_TH=0.55
+#     VEH_TH=$i
+#     echo "testing with ROAD_TH=$ROAD_TH, VEH_TH=$VEH_TH"
+#     echo "WY_echo" | grader "python run.py $ROAD_TH $VEH_TH" | grep "Car F score:" | (echo -n "road_th: $ROAD_TH | vehicle_th: $VEH_TH | " && cat) >> log_hyper_tune.txt
+# done 
+
