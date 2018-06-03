3b010cc625de76ec684479c40f9e6e88f95869ac
Yang Wang <yang@1536-laptop.local>
2018-06-03 15:08:05 -0700

    sync

--- Keras-FCN/tune.sh
+++ Keras-FCN/tune.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
-for i in `seq 0.23 0.01 0.33`;
+for i in `seq 0.0 0.03 0.3`;
 do
     ROAD_TH=0.57
     ROAD_FADE=0
 
-    VEH_TH=$i
-    VEH_FADE=0
+    VEH_TH=0.26
+    VEH_FADE=$i
     echo "testing with ROAD_TH=$ROAD_TH, VEH_TH=$VEH_TH"
     echo "WY_echo" | grader "python inference_client.py $ROAD_TH $VEH_TH $ROAD_FADE $VEH_FADE" | grep "Car F score:" | (echo -n "road_th: $ROAD_TH | road_fade: $ROAD_FADE | veh_th: $VEH_TH | veh_fade: $VEH_FADE | " && cat) >> log_hyper_tune.txt
 done 
@@ -28,3 +28,6 @@ done
 #road_th: 0.57 | road_fade: 0 | veh_th: 0.20 | veh_fade: 0.05 | Car F score: 0.792 | Car Precision: 0.544 | Car Recall: 0.894 | Road F score: 0.976 | Road Precision: 0.987 | Road Recall: 0.933 | Averaged F score: 0.884
 # veh_th 0.2, veh_fade 0.0-0.05, car_f 0.794
 
+#road_th: 0.57 | road_fade: 0 | veh_th: 0.24 | veh_fade: 0 | Car F score: 0.798 | Car Precision: 0.600 | Car Recall: 0.870 | Road F score: 0.976 | Road Precision: 0.987 | Road Recall: 0.933 | Averaged F score: 0.887
+#road_th: 0.57 | road_fade: 0 | veh_th: 0.25 | veh_fade: 0 | Car F score: 0.798 | Car Precision: 0.610 | Car Recall: 0.864 | Road F score: 0.976 | Road Precision: 0.987 | Road Recall: 0.933 | Averaged F score: 0.887
+# veh_th 0.24-0.25, veh_fade 0, car_f 0.798
