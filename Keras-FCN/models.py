f54d5c83bfe9df95397a5c30a6cf1c002242419f
Yang Wang <yang@1536-laptop.local>
2018-06-03 17:28:36 -0700

    sync

--- Keras-FCN/models.py
+++ Keras-FCN/models.py
@@ -39,14 +39,11 @@ def zerg_model(weight_decay=0., batch_momentum=0.9, batch_shape=[20, 320, 320, 3
     x = conv_block(3, [64, 64, 256], stage=2, block='a', weight_decay=weight_decay, strides=(1, 1), batch_momentum=batch_momentum)(x)
     x = identity_block(3, [64, 64, 256], stage=2, block='b', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
     x = identity_block(3, [64, 64, 256], stage=2, block='c', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
-    x = identity_block(3, [64, 64, 256], stage=2, block='d', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
 
     x = conv_block(3, [128, 128, 512], stage=3, block='a', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
     x = identity_block(3, [128, 128, 512], stage=3, block='b', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
     x = identity_block(3, [128, 128, 512], stage=3, block='c', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
     x = identity_block(3, [128, 128, 512], stage=3, block='d', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
-    x = identity_block(3, [128, 128, 512], stage=3, block='e', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
-    x = identity_block(3, [128, 128, 512], stage=3, block='f', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
 
     x = conv_block(3, [256, 256, 1024], stage=4, block='a', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
     x = identity_block(3, [256, 256, 1024], stage=4, block='b', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
@@ -54,8 +51,6 @@ def zerg_model(weight_decay=0., batch_momentum=0.9, batch_shape=[20, 320, 320, 3
     x = identity_block(3, [256, 256, 1024], stage=4, block='d', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
     x = identity_block(3, [256, 256, 1024], stage=4, block='e', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
     x = identity_block(3, [256, 256, 1024], stage=4, block='f', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
-    x = identity_block(3, [256, 256, 1024], stage=4, block='g', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
-    x = identity_block(3, [256, 256, 1024], stage=4, block='h', weight_decay=weight_decay, batch_momentum=batch_momentum)(x)
 
     x = atrous_conv_block(3, [512, 512, 2048], stage=5, block='a', weight_decay=weight_decay, atrous_rate=(2, 2), batch_momentum=batch_momentum)(x)
     x = atrous_identity_block(3, [512, 512, 2048], stage=5, block='b', weight_decay=weight_decay, atrous_rate=(2, 2), batch_momentum=batch_momentum)(x)
